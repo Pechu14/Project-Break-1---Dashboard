985b16fc9969191c8961a1e0f55db05b54e4429c
Pechu14 <jesus.gil.santos1992@gmail.com>
2024-07-16 12:46:33 +0200

    reloj con frase

--- js/script-clock.js
+++ js/script-clock.js
@@ -1,4 +1,5 @@
 const datos = new Date()
+const divReloj = document.getElementById("reloj");
 
  function formatearFecha(fecha) {
     let dia = fecha.getDate()
@@ -33,13 +34,55 @@ const datos = new Date()
  }
 
  function mostrarReloj(){
- const divReloj = document.getElementById("reloj");
+ 
  const datos = new Date();
-  divReloj.innerHTML = '<div>' + formatearHora(datos) + '</div>' +
-                        '<div>' + formatearFecha(datos) + '</div>';
+  divReloj.innerHTML = '<div id = horafecha>' +
+                            '<div>' + formatearHora(datos) + '</div>' +
+                            '<div>' + formatearFecha(datos) + '</div>' +
+                        '</div>'
  }
  setInterval(mostrarReloj, 1000);
  mostrarReloj();
+
+
+ function mostrarFrase(horas){
+    const horaFecha = document.getElementById("horaFecha");
+    const divFrase = document.getElementById("frase")
+    if(horas >= 0 && horas < 7){
+        divFrase.innerHTML = '<p>Hora de dormir</p>'
+    }                 
+    else if(horas >= 7 && horas < 10 ){
+        divFrase.innerHTML ='<p>A pensar y programar</p>'
+    }
+     else if(horas >= 10 && horas < 11 ){
+        divFrase.innerHTML ='<p>Toca desayunar</p>'
+    }
+     else if(horas >= 11 && horas < 14 ){
+        divFrase.innerHTML ='<p>A seguir programando</p>'
+    }
+     else if(horas >= 14 && horas < 16 ){
+        divFrase.innerHTML ='<p>Come y descansa</p>'
+    }
+     else if(horas >= 16 && horas < 18 ){
+        divFrase.innerHTML ='<p>Animo que ya no queda nada</p>'
+    }
+    else if(horas >= 18 && horas < 19 ){
+        divFrase.innerHTML ='<p>Haz un descansito</p>'
+    }
+    else if(horas >= 19 && horas < 21 ){
+        divFrase.innerHTML ='<p>Ultimo tramo</p>'
+    }
+    else if(horas >= 21 && horas < 24 ){
+        divFrase.innerHTML ='<p>Mañana será otro día</p>'
+    }
+}
+divReloj.addEventListener("click", () => mostrarFrase(datos.getHours()))
+const divFrase = document.getElementById("frase")
+function borarFrase() {
+    divFrase.innerHTML=""
+}
+divFrase.addEventListener("click", () => borarFrase())
+
 
  
 
